00fe3b76edc5722766400d941ec30de176d79b2d
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-05-01 21:22:57 +0530

    sidebar is completed

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -5,25 +5,36 @@ import {
   faCalculator,
   faChalkboardTeacher,
   faClipboardList,
+  faClockRotateLeft,
   faDashboard,
   faGraduationCap,
   faListDots,
   faMessage,
+  faMoneyBillTransfer,
+  faMoneyBillTrendUp,
+  faPlus,
   faUserGraduate,
   faUserGroup,
   faUserPlus,
+  faUsers,
 } from "@fortawesome/free-solid-svg-icons";
 import "../css/sidebar.css";
 import { faUserCheck } from "@fortawesome/free-solid-svg-icons/faUserCheck";
+import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons/faPeopleGroup";
+import { faMoneyBills } from "@fortawesome/free-solid-svg-icons/faMoneyBills";
+import { faList12 } from "@fortawesome/free-solid-svg-icons/faList12";
 
 const SideBar = () => {
   const value = useContext(AppContext);
-  const [activeBar, setactiveBar] = useState(null);
+  const [activeBar, setactiveBar] = useState("dashboard");
   return (
     <>
       <aside className={`sidebar ${value.isSidebar ? "active" : ""}`}>
         <ul className="sidebar">
-          <li className={`side_menu`}>
+          <li
+            className={`side_menu ${activeBar === "dashboard" ? "active" : ""}`}
+            onClick={() => setactiveBar("dashboard")}
+          >
             <div className="inner_menu">
               <div className="side_icon">
                 <FontAwesomeIcon icon={faDashboard} />
@@ -78,6 +89,32 @@ const SideBar = () => {
               </div>
               <div className="side_text">Teachers</div>
             </div>
+            <ul className="sub_menu">
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faPeopleGroup} />
+                  </div>
+                  <p className="submenu_text">All Teachers</p>
+                </div>
+              </li>
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faUserPlus} />
+                  </div>
+                  <p className="submenu_text">Add Teacher</p>
+                </div>
+              </li>
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faMoneyBills} />
+                  </div>
+                  <p className="submenu_text">Payment</p>
+                </div>
+              </li>
+            </ul>
           </li>
           <li
             className={`side_menu ${activeBar === "parents" ? "active" : ""}`}
@@ -89,6 +126,32 @@ const SideBar = () => {
               </div>
               <div className="side_text">Parents</div>
             </div>
+            <ul className="sub_menu">
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faPeopleGroup} />
+                  </div>
+                  <p className="submenu_text">All Parents</p>
+                </div>
+              </li>
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faUserPlus} />
+                  </div>
+                  <p className="submenu_text">Add Parent</p>
+                </div>
+              </li>
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faUsers} />
+                  </div>
+                  <p className="submenu_text">Meeting</p>
+                </div>
+              </li>
+            </ul>
           </li>
           <li
             className={`side_menu ${activeBar === "acconunt" ? "active" : ""}`}
@@ -100,6 +163,32 @@ const SideBar = () => {
               </div>
               <div className="side_text">Acconunt</div>
             </div>
+            <ul className="sub_menu">
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faMoneyBillTrendUp} />
+                  </div>
+                  <p className="submenu_text">Fess Collections</p>
+                </div>
+              </li>
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faMoneyBillTransfer} />
+                  </div>
+                  <p className="submenu_text">Expenses</p>
+                </div>
+              </li>
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faPlus} />
+                  </div>
+                  <p className="submenu_text">Add Expenses</p>
+                </div>
+              </li>
+            </ul>
           </li>
           <li
             className={`side_menu ${activeBar === "exam" ? "active" : ""}`}
@@ -111,6 +200,24 @@ const SideBar = () => {
               </div>
               <div className="side_text">Exam</div>
             </div>
+            <ul className="sub_menu small_submenu">
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faList12} />
+                  </div>
+                  <p className="submenu_text">All Exams</p>
+                </div>
+              </li>
+              <li className="sub_menu_li">
+                <div className="inner_submenu">
+                  <div className="submenu_icon">
+                    <FontAwesomeIcon icon={faClockRotateLeft} />
+                  </div>
+                  <p className="submenu_text">Exam Scheduke</p>
+                </div>
+              </li>
+            </ul>
           </li>
           <li
             className={`side_menu ${activeBar === "notice" ? "active" : ""}`}
