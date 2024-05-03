964b0259891240cdc9504d25edb237208e80b4c4
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-05-03 21:38:29 +0530

    student promotion is ready

--- src/Components/Studentpromotion.jsx
+++ src/Components/Studentpromotion.jsx
@@ -17,7 +17,9 @@ const Studentpromotion = () => {
               <div className="options">
                 <select name="current_session" id="current_session">
                   <option value="">class</option>
-                  <option value="1">1</option>
+                  <option className="option" value="1" selected>
+                    1
+                  </option>
                   <option value="2">2</option>
                   <option value="3">3</option>
                   <option value="4">4</option>
@@ -32,12 +34,84 @@ const Studentpromotion = () => {
                 </select>
                 <select name="promotion_session" id="promotion_session">
                   <option value="">section</option>
-                  <option value="a">A</option>
+                  <option value="a" selected>
+                    A
+                  </option>
                   <option value="b">B</option>
                   <option value="c">C</option>
                   <option value="d">D</option>
                 </select>
+                <button type="button" className="fetch">
+                  Fetch
+                </button>
               </div>
+              <table className="table promotetable">
+                <thead>
+                  <tr>
+                    <th>
+                      <input type="checkbox" name="" id="" />
+                    </th>
+                    <th>Roll no</th>
+                    <th>name</th>
+                    <th>class</th>
+                    <th>section</th>
+                    <th>status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="" id="" />
+                    </td>
+                    <td>#0001</td>
+                    <td>john doe</td>
+                    <td>1</td>
+                    <td>A</td>
+                    <td>
+                      <select name="status" id="status">
+                        <option value="promote" selected>
+                          Promote
+                        </option>
+                        <option value="depromote">Depromote</option>
+                      </select>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="" id="" />
+                    </td>
+                    <td>#0001</td>
+                    <td>john doe</td>
+                    <td>1</td>
+                    <td>A</td>
+                    <td>
+                      <select name="status" id="status">
+                        <option value="promote" selected>
+                          Promote
+                        </option>
+                        <option value="depromote">Depromote</option>
+                      </select>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type="checkbox" name="" id="" />
+                    </td>
+                    <td>#0001</td>
+                    <td>john doe</td>
+                    <td>1</td>
+                    <td>A</td>
+                    <td>
+                      <select name="status" id="status">
+                        <option value="promote" selected>
+                          Promote
+                        </option>
+                        <option value="depromote">Depromote</option>
+                      </select>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </section>
