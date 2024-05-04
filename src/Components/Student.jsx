8f565f59e6017367ed88e139db482cd746c26448
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-05-04 23:33:07 +0530

    setting back to normal

--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Student = () => {
-  return <div>Student</div>;
+  return (
+    <>
+      <h1>Setting basic student</h1>
+      <h1>Setting basic student</h1>
+    </>
+  );
 };
 
 export default Student;
