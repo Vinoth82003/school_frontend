24501583823ec48854646e8b3d4d5a777244f1a1
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2024-05-07 15:26:29 +0530

    Small Changes in Message component

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,135 +1,189 @@
 import React from "react";
+import "../css/message.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCameraAlt,
+  faExclamationTriangle,
+  faFileArrowUp,
+  faPaperPlane,
+  faPaperclip,
+  faSearch,
+  faSmile,
+  faUserAltSlash,
+} from "@fortawesome/free-solid-svg-icons";
 import photo1 from "../img/student11 (1).png";
 import photo2 from "../img/student11.png";
 import photo3 from "../img/student12.png";
 import photo4 from "../img/student13.png";
-import "../css/message.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faExclamationTriangle,
-  faUserCircle,
-  faUserSlash,
-} from "@fortawesome/free-solid-svg-icons";
 
 const Message = () => {
-  const ContactDetails = () => {
-    const photos = [photo1, photo2, photo3, photo4];
-    const times = ["11:20 PM", "12:20 AM", "01:00 AM", "10:00 AM"];
-    const contacts = [];
+  // creating the contact list
+  const ContactList = () => {
+    let contacts = [];
+    let name = ["John Doe", "Lorem Ispum", "Milly Jane", "Henry Jones"];
+    let proff = ["Teacher", "Student", "Parent", "Admin"];
+    let photos = [photo1, photo2, photo3, photo4];
+    let times = ["11:00 AM", "12:00 PM", "10:00 AM", "01:00 PM", "08:10 AM"];
+    let counts = [6, 10, 100, 11, 1, 55];
+    let isBlocked = [true, false];
 
-    for (let i = 1; i <= 10; i++) {
-      let image = photos[Math.floor(Math.random() * photos.length)];
-      let roll = i < 10 ? `#000${i}` : `#00${i}`;
-      let name = `Student_${roll}`;
-      let time = times[Math.floor(Math.random() * times.length)];
-
+    for (let i = 0; i < 10; i++) {
+      let cName = name[Math.floor(Math.random() * name.length)];
+      let cProff = proff[Math.floor(Math.random() * proff.length)];
+      let cPhoto = photos[Math.floor(Math.random() * photos.length)];
+      let cTimes = times[Math.floor(Math.random() * times.length)];
+      let cCounts = counts[Math.floor(Math.random() * counts.length)];
+      let cisBlocked = isBlocked[Math.floor(Math.random() * isBlocked.length)];
       let data = {
-        image,
-        roll,
-        name,
-        time,
+        cName,
+        cProff,
+        cPhoto,
+        cTimes,
+        cCounts,
+        cisBlocked,
       };
       contacts.push(data);
     }
 
-    return contacts.map((contact, index) => (
-      <li key={index} className="list contact">
-        <div className="inner_list contact_list">
-          <div className="user_profile">
-            <img src={contact.image} alt="user logo" />
+    return contacts.map((contact, index) => {
+      return (
+        <li
+          key={index}
+          className={`contact list ${
+            contact.cisBlocked ? "blocked" : "unblocked"
+          }`}
+        >
+          <div className="inner_contact">
+            <div className="user_profile">
+              <img src={contact.cPhoto} alt="user profile" />
+            </div>
+            <div className="user_name">
+              <p className="uname">{contact.cName}</p>
+              <span className="proffesion">{contact.cProff}</span>
+            </div>
+            <div className="time_count">
+              <p className="time">{contact.cTimes}</p>
+              <div className="count">
+                {contact.cCounts < 10 ? "0" + contact.cCounts : contact.cCounts}
+              </div>
+            </div>
           </div>
-          <div className="user_name">
-            <div className="roll">{contact.roll}</div>
-            <div className="StudName">{contact.name}</div>
-          </div>
-          <p className="time">{contact.time}</p>
-        </div>
-      </li>
-    ));
+        </li>
+      );
+    });
   };
 
-  const messages = () => {
-    let message_box = [];
-    let times = ["11:00 AM", "10:00 AM", "01:00 PM", "12:00 AM", "12:00 PM"];
-    let types = ["sent", "recieve"];
-    let messages = [
-      "this is first message ",
-      "this is second message this is first message ",
-      "this is second message this is first message ",
-      "this is second message this is first message this is second message this is first message this is second message this is first message this is second message this is first message ",
-    ];
-    for (let i = 0; i <= 10; i++) {
-      let time = times[Math.floor(Math.random() * times.length)];
-      let type = types[Math.floor(Math.random() * types.length)];
-      let message = messages[Math.floor(Math.random() * messages.length)];
-      let data = {
-        time,
-        type,
-        message,
-      };
+  // creating sample message box
 
-      message_box.push(data);
-    }
-
-    return message_box.map((message, index) => (
-      <li key={index} className={`message_li ${message.type}`}>
-        <div className="inner_message_li">
-          <p className="msg">{message.message}</p>
-          <p className="time">{message.time}</p>
-        </div>
-      </li>
-    ));
-  };
-
+  // return the message component
   return (
     <>
       <main className="admin_page">
         <section className="chart_containers">
-          <div className="table_box message_box">
-            <div className="message_left">
-              <ul className="message_top">
-                <li className="tag teacher">Teachers</li>
-                <li className="tag student">Students</li>
-              </ul>
-              <ul className="contact_list_ul">
-                <ContactDetails />
-              </ul>
-            </div>
-            <div className="message_conversation">
-              <div className="conversation_top">
-                <div className="conv_prof">
-                  <div className="user_profile">
-                    <img src={photo1} alt="prof" />
-                  </div>
-                  <div className="user_name">
-                    <div className="roll">#1212</div>
-                    <div className="StudName">Jancy Jone</div>
+          <div className="message_box">
+            <div className="inner_message_box">
+              <div className="chat_contact">
+                <div className="contact_top">
+                  <div className="search">
+                    <input
+                      type="text"
+                      name="seearcContact"
+                      id="seearcContact"
+                    />
+                    <div className="search_icon">
+                      <FontAwesomeIcon icon={faSearch} />
+                    </div>
                   </div>
                 </div>
-                <div className="options">
-                  <div className="msgOptList block">
-                    <FontAwesomeIcon icon={faUserSlash} />
-                    <p className="text_hover">Block</p>
+                <ul className="contacts">
+                  <ContactList />
+                </ul>
+              </div>
+              <div className="chat_message">
+                <div className="message_top">
+                  <div className="message_top_prof">
+                    <div className="user_profile">
+                      <img src={photo1} alt="user profile" />
+                    </div>
+                    <div className="user_name">
+                      <h3 className="uname">John Doe</h3>
+                      <span className="proffesion">Student</span>
+                    </div>
                   </div>
-                  <div className="msgOptList report">
-                    <FontAwesomeIcon icon={faExclamationTriangle} />
-                    <p className="text_hover">Report</p>
+                  <div className="options">
+                    <button className="msg_opt">
+                      <FontAwesomeIcon icon={faUserAltSlash} />
+                      <span className="btn-text">block</span>
+                    </button>
+                    <button className="msg_opt">
+                      <FontAwesomeIcon icon={faExclamationTriangle} />
+                      <span className="btn-text">report</span>
+                    </button>
+                    <button className="msg_opt">
+                      <FontAwesomeIcon icon={faExclamationTriangle} />
+                      <span className="btn-text">report</span>
+                    </button>
                   </div>
-                  <div className="msgOptList view">
-                    <FontAwesomeIcon icon={faUserCircle} />
-                    <p className="text_hover">View</p>
+                </div>
+                <ul className="convo_box">
+                  <li className="msg info">info message</li>
+                  <li className="msg recievedMsg">
+                    this is the 1st message from he my frient
+                    <span className="time">11:00 AM</span>
+                  </li>
+                  <li className="msg recievedMsg">
+                    this is the 1st message from he my frient
+                    <span className="time">11:00 AM</span>
+                  </li>
+                  <li className="msg sentMsg">
+                    this is the message sent to my frient this is the 1st
+                    message from he my frient this is the 1st message from he my
+                    frient this is the 1st message from he my frient
+                    <span className="time">11:00 AM</span>
+                  </li>
+                  <li className="msg recievedMsg">
+                    this is the 1st message from he my frient
+                    <span className="time">11:00 AM</span>
+                  </li>
+                  <li className="msg sentMsg">
+                    this is the 1st message from he my frient
+                    <span className="time">11:00 AM</span>
+                  </li>
+                  <li className="msg recievedMsg">
+                    this is the 1st message from he my frient
+                    <span className="time">11:00 AM</span>
+                  </li>
+                  <li className="msg sentMsg">
+                    this is the 1st message from he my frient
+                    <span className="time">11:00 AM</span>
+                  </li>
+                  <li className="msg recievedMsg">
+                    this is the message sent to my frient this is the 1st
+                    message from he my frient this is the 1st message from he my
+                    frient this is the 1st message from he my frient
+                    <span className="time">11:00 AM</span>
+                  </li>
+                </ul>
+                <div className="message_send_box">
+                  <div className="input_box">
+                    <input type="text" id="sendMsg" className="sendMsg" />
+                    <button type="button" className="inp-icon">
+                      <FontAwesomeIcon icon={faPaperPlane} />
+                    </button>
+                  </div>
+                  <div className="msg_opts">
+                    <button type="button" className="file_input">
+                      <FontAwesomeIcon icon={faSmile} />
+                    </button>
+                    <button type="button" className="file_input">
+                      <FontAwesomeIcon icon={faPaperclip} />
+                    </button>
+                    <button type="button" className="file_input">
+                      <FontAwesomeIcon icon={faCameraAlt} />
+                    </button>
                   </div>
                 </div>
               </div>
-              <ul className="convo_box">
-                <li className="message_li info">info</li>
-                {messages()}
-              </ul>
-              <ul className="message_send_input">
-                <li className="input_box">
-                  <input type="text" name="send message" id="send message" />
-                </li>
-              </ul>
             </div>
           </div>
         </section>
